d074abf96b7a260a40aec913e4c65ee8042eb1cf
laguinefe guinee <laguinefe224@gmail.com>
2025-08-21 06:30:00 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,13 @@ import { getAuth, Auth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  "projectId": "artisans-compass",
+  "appId": "1:643243336578:web:8e4c52a92c9948b41da59b",
+  "storageBucket": "artisans-compass.firebasestorage.app",
+  "apiKey": "AIzaSyDj4h-kGIo8gRdf_Gf5_8Tr92NCHERJdiw",
+  "authDomain": "artisans-compass.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "643243336578"
 };
 
 // Initialize Firebase
